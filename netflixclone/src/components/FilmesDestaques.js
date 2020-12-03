3ac0421ab1db76e15df40a9abb73c4b046ebf997
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-03 11:38:27 -0300

    parte 27

--- netflixclone/src/components/FilmesDestaques.js
+++ netflixclone/src/components/FilmesDestaques.js
@@ -6,8 +6,9 @@ import './FilmesDestaques.css'
 export default function FilmesDestaques({item}) {
 
       return (
-          <section className="destacado">
+          <section className="destacado" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`}}>
                <div>{item.original_name}</div>
+               
           </section>
       )
 }
