9c688318fe67b0e8c66ffb91c88070221e21edfa
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-03 12:27:26 -0300

    parte 30

--- netflixclone/src/components/FilmesDestaques.js
+++ netflixclone/src/components/FilmesDestaques.js
@@ -10,7 +10,13 @@ export default function FilmesDestaques({item}) {
                {/* <div>{item.original_name}</div> */}
                <div className="destacado--vertical">
                    <div className="destacado--horizontal">
-                        
+                        <div className="destacado--nome">{item.original_name}</div>
+                        <div className="destacado--info">
+                            <div className="destacado--pontos">{item.vote_average} pontos</div>
+                            <div className="destacado--ano">2099</div>
+                            <div className="destacado-temporadas">{item.number_of_seasons} temporadas</div>
+                            <div>{}</div>
+                        </div>
                    </div>
                </div>
           </section>
