95db7edcd20895a324d546f22d87b02b96336702
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-03 10:16:16 -0300

    parte 21

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import Tmdb from './Tmdb'
 import ListaFilmes from './components/LinhaFilmes'
+import FilmesDestaques from './components/FilmesDestaques'
 
 
 export default function App() {
@@ -20,6 +21,8 @@ export default function App() {
 
     return (
       <div className="pagina">
+          <FilmesDestaques />
+
           <section className="listas">
                {listaFilmes.map((item, chave) => (
                    <ListaFilmes key={chave} titulo={item.titulo} itens={item.itens} />
---
+++ netflixclone/src/components/FilmesDestaques.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './FilmesDestaques.css'
+
+
+
+export default function FilmesDestaques() {
+
+      return (
+          <div>
+              Filme em destaque
+          </div>
+      )
+}
+
