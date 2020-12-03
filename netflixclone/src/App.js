26fcc699041206f49d323be8a2d04fb24dfdea10
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-02 22:10:08 -0300

    parte 7

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,22 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Tmdb from './Tmdb'
 
 
 
 export default function App() {
+
+   const [listaFilmes, setListaFilmes] = useState([])
+   
    useEffect(() => {
         const carregarTudo = async () => {
             // pegando a lista total
             let lista = await Tmdb.getListaCaseira()
             console.log(lista)
+            setListaFilmes(lista)
         }
         carregarTudo()
     }, [])
 
     return (
-      <div>
-          Olá Mundo!
+      <div className="pagina">
+          <section className="listas">
+               {listaFilmes.map((item, chave) => (
+                   <div>
+                      {item.titulo}
+                   </div>
+               ))}
+          </section>          
       </div>
     )
 }
