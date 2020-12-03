7179ed35c0ec00ad333b209dc60d4326a389eb8d
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-02 22:41:35 -0300

    parte 11

--- netflixclone/src/components/LinhaFilmes.js
+++ netflixclone/src/components/LinhaFilmes.js
@@ -8,8 +8,14 @@ export default function LinhaFilmes({ titulo, itens }) {
      return (
         <div>
            <h2>{titulo}</h2>
+           <div className="listaFilmes--arealista">
+                {itens.results.length > 0 && itens.results.map((item, chave) => (
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} key={chave}/>
+                ))}
+           </div>
         </div>
      )
+
 }
 
 
