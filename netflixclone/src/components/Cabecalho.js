d8508d3a9d932cc0fe89e879ee4ec93284320e62
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-07 13:02:01 -0300

    parte 41

--- netflixclone/src/components/Cabecalho.js
+++ netflixclone/src/components/Cabecalho.js
@@ -6,7 +6,16 @@ export default function Cabecalho() {
 
      return (
          <header>
-            ....
+             <div className="cabecalho--logo">
+                 <a href="/">
+                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix logo" />
+                 </a>
+             </div>
+             <div className="cabecalho--usuario">
+                  <a href="/">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Grey_user_add_icon_%28Wikiproject_icons%29.svg" alt="" />
+                  </a>
+             </div>
          </header>
      )
 
