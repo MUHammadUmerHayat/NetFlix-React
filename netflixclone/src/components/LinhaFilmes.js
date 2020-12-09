90cfff6e04247b2559ba10d6758d62c07dc19b07
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-09 11:53:08 -0300

    parte 59

--- netflixclone/src/components/LinhaFilmes.js
+++ netflixclone/src/components/LinhaFilmes.js
@@ -7,18 +7,26 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 export default function LinhaFilmes({ titulo, itens }) {
 
+     const direcionarSetaEsquerda = () => {
+
+     }
+
+     const direcionarSetaDireita = () => {
+          
+     }
+
      return (
         <div className="listaFilmes">
            <h2>{titulo}</h2>
-           <div className="listaFilmes--esquerdo">
+           <div className="listaFilmes--esquerdo" onClick={direcionarSetaEsquerda}>
                 <ArrowBackIosIcon style={{fontSize: 50}} />
            </div>
-           <div className="listaFilmes--direito">
+           <div className="listaFilmes--direito" onClick={direcionarSetaDireita}>
                 <ArrowForwardIosIcon style={{fontSize: 50}} />
            </div>
 
            <div className="listaFilmes--arealista">
-                <div className="listaFilmes--lista">
+                <div className="listaFilmes--lista" style={{marginLeft: -400}}>
                     {itens.results.length > 0 && itens.results.map((item, chave) => (
                         <div className="listaFilmes--item" key={chave}>
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} key={chave} alt={item.original_title}/>
